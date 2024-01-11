d42d995669a0f32acb46bb533fd83045c9a1dd09
kds98 <skyudae@naver.com>
2024-01-11 13:35:06 +0900

    init

---
+++ src/main/java/com/ll/sbb20230111/Sbb20230111Application.java
@@ -0,0 +1,13 @@
+package com.ll.sbb20230111;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sbb20230111Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sbb20230111Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/ll/sbb20230111/Sbb20230111ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.sbb20230111;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sbb20230111ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
