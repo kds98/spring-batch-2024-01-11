51ddfdfbf5c2ae8ac2cd6660e1768c020ca12422
kds98 <skyudae@naver.com>
2024-01-11 15:50:13 +0900

    2

--- src/test/java/com/ll/sbb20240111/Sbb20240111ApplicationTests.java
+++ src/test/java/com/ll/sbb20240111/Sbb20240111ApplicationTests.java
@@ -1,10 +1,10 @@
-package com.ll.sbb20230111;
+package com.ll.sbb20240111;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Sbb20230111ApplicationTests {
+class Sbb20240111ApplicationTests {
 
     @Test
     void contextLoads() {
